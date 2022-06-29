5e0ba25392330976f39bcca5a1852a30cd4b1575
Suhas Bharadwaj K <tak.kumara@gmail.com>
2022-06-29 23:14:14 +0530

    Articles Updated

--- Articles.py
+++ Articles.py
@@ -1,5 +1,22 @@
 Articles = {
 
+    "12": "Defines what is state in Union of India. No state government can take a decision that they will form a new state. All organisations and units of state will be considered under union",
+    "13": "Any law which contradicts the provisions of fundamental rights stands invalid or void",
+    "32": "Supreme Court has the right to declare any law to be invalid if it contradicts the provision of fundamental rights",
+    "226": "High Court has the right to declare any law to be invalid if it contradicts the provision of fundamental rights",
+    
+    #Right to Equality
+    "14": "Equality before law",
+    "15": "Prohibition of discrimination on religion, race, caste and birth place",
+    "16": "Equality of opportunity for public employment",
+    "17": "Abolition of untouchability",
+    "18": "Abolition of titles like Maharaja, Sultan, etc.",
+
+    #Right to Freedom. Article 20 and 21 cannot be restricted at any point of time
+    "19": "Provides following seven rights: a. Right to Speech b. Right to conduct meetings in non-violent manner c. Right to form Union d. Freedom of Movement e. Freedom for residence and settlement (Exception: i. Jammu and Kashmir ii. Places where military resides) f. Freedom of profession, occupation, trade or business g. Right to use internet",
+    "20": "Protection in respect of conviction for offenses (An offender will get only that much of punishment given by court. E.g. If an offender gets 6 months of conviction, police cannot extend his conviction for 2 years due to personal grudge )",
+    "21": "Protection of life and personal liberty with following rights: a. Right to privacy b. Right to live c. Right to health d. Right to education till 14 years of age (Important) e. Right for free help of law f. Right against seperate jail g. Right against handcuffs h. Right against violence nature g. Right against the late hanging h. Right to travel foreign i. Right against to become a bonded labour j. Right against hanging publicly k. Right to sleep l. Right against pollution m. Right for electricity n. Right to behave with respect to women",
+    "22": "Protection against arrest and detention in certain cases",
 
 
 
