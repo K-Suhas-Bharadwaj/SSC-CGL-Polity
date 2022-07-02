f816efe99523bd772d5ceb4dfae961b9bd86e497
Suhas Bharadwaj K <tak.kumara@gmail.com>
2022-07-02 13:55:01 +0530

    Commit

--- Articles.py
+++ Articles.py
@@ -1,4 +1,4 @@
-Articles = {
+Fundamental_Rights_Articles = {
 
     "12": "Defines what is state in Union of India. No state government can take a decision that they will form a new state. All organisations and units of state will be considered under union",
     "13": "Any law which contradicts the provisions of fundamental rights stands invalid or void",
@@ -6,7 +6,7 @@ Articles = {
     "226": "High Court has the right to declare any law to be invalid if it contradicts the provision of fundamental rights",
     
     #Right to Equality
-    "14": "Equality before law",
+    "14": "Equality before law. Applies to foreigners as well",
     "15": "Prohibition of discrimination on religion, race, caste and birth place",
     "16": "Equality of opportunity for public employment",
     "17": "Abolition of untouchability",
@@ -14,18 +14,19 @@ Articles = {
 
     #Right to Freedom. Article 20 and 21 cannot be restricted at any point of time
     "19": "Provides following seven rights: a. Right to Speech b. Right to conduct meetings in non-violent manner c. Right to form Union d. Freedom of Movement e. Freedom for residence and settlement (Exception: i. Jammu and Kashmir ii. Places where military resides) f. Freedom of profession, occupation, trade or business g. Right to use internet",
-    "20": "Protection in respect of conviction for offenses (An offender will get only that much of punishment given by court. E.g. If an offender gets 6 months of conviction, police cannot extend his conviction for 2 years due to personal grudge )",
-    "21": "Protection of life and personal liberty with following rights: a. Right to privacy b. Right to live c. Right to health d. Right to education till 14 years of age (Important) e. Right for free help of law f. Right against seperate jail g. Right against handcuffs h. Right against violence nature g. Right against the late hanging h. Right to travel foreign i. Right against to become a bonded labour j. Right against hanging publicly k. Right to sleep l. Right against pollution m. Right for electricity n. Right to behave with respect to women",
-    "22": "Protection against arrest and detention in certain cases",
+    "20": "Protection in respect of conviction for offenses (An offender will get only that much of punishment given by court. E.g. If an offender gets 6 months of conviction, police cannot extend his conviction for 2 years due to personal grudge ). Applies to foreigners as well.",
+    "21": "Protection of life and personal liberty with following rights: a. Right to privacy b. Right to live c. Right to health d. Right to education till 14 years of age (Important) e. Right for free help of law f. Right against seperate jail g. Right against handcuffs h. Right against violence nature g. Right against the late hanging h. Right to travel foreign i. Right against to become a bonded labour j. Right against hanging publicly k. Right to sleep l. Right against pollution m. Right for electricity n. Right to behave with respect to women. All these rights listed under Article 21 applies to foreigners as well",
+    "21A": "Right to initial education. Applies to foreigners as well."
+    "22": "Protection against arrest and detention in certain cases. Applies to foreigners as well.",
 
     #Right against Exploitation
     "23": "Prohibition of traffic in human beings and forced labour with following provisions: a. Provision of unethical business of children and women b. Slavery System c. Prohibition of unbound labour",
     "24": "Prohibition of employment of children in factories. There shall not be labour below 14 years of age. In 1996, Supreme Court Ordered that there shall be an establishment of Children Welfare Account",
 
     #Right to Freedom of Religion
-    "25": "Freedom of conscience and free practice and propagation of religion",
+    "25": "Freedom of conscience and free practice and propagation of religion. Applies to foreigners as well,
     "26": "Freedom to manage religious affairs",
-    "27": "Freedom of taxes for payments to promote religious affairs",
+    "27": "Freedom of taxes for payments to promote religious affairs. Applies to foreigners as well",
     "28": "Freedom to attend religious worship in certain educational institution a. Educational institutions undertaken by government - Completely Prohibited b. Educational institutions undertaken by government but established for special purposes - Religious education granted. c. Educational institutions approved by government - Free to attend religious education d. Educational institutions which are financially healed by government - Free to attend religious education",
 
     #Right for Culture and Education
@@ -34,7 +35,40 @@ Articles = {
 
     #Right to property ==> Article 31 ==> No more a fundamental right ==> Hade become a legal right in 1978 by the means of 44th Ammendment 
     
-    #Right to Constitutional Remedy
-    "32": ""
+    #Right to Constitutional Remedy (Soul of Indian Constitution)
+    "32": "Right to have constitutional remedies when one's fundamental right is violated or transgressed",
+    "33": "Parliament can give rights to Indian Arm Force and other such institutions to restrict or modify fundamental rights for some time",
+    "34": "Restriction on Fundamental Rights while martial law is in force in any area",
+    "35": "Legislation to give effect to the provisions of Funamental Rights"
+
+}
+
+Articles_Directive_Principles_Of_State_Policy = {
+
+    "36": "Definition of Directive Principles of State Policy",
+    "37": "Application of Directive Principles of State Policy",
+    "38": "States should secure a social order for the promotion of the welfare of the people. All the citizens of the state shall get social, financial and political justice",
+    "39": "States should follow all the directive principles of state policy",
+    "39A": "a. Equal justice to everyone and free legal aid (proceedings of court is free to everyone) b. Equal pay for Equal Work (Should not be the case that men are paid more than women for same work) c. Handle citizen’s money such that the least person of the society gets benefited from it (i.e. Revenue collected should be handled at its best)",
+    "39B": "Equal distribution of revenue. E.g. If a government school is giving admissions only to Muslims, then it is not equal distribution of revenue. These situations should not arise in the state",
+    "40": "A state should implement Panchayat Raj. NOTE: There is difference between Article 40 and Article 243. Article 243 ==> Provision to have Panchayat Raj. Article 40 ==> Directions to state to implement Panchayat Raj",
+    "41": "Right to work, to education and to public assistance in certain cases. Public Assistance: Example: A farmer takes a loan an couldn’t repay due to certain reasons. In this case, the bank files a case in court of law. The judge after hearing the problems of farmer, will direct bank to negotiate with the farmer. At certain point, bank agrees to clear the loan if farmer repays half of the debt. This is called Public Assistance”,
+    "42": "Provision for justice at work. Provision for having humane conditions at work. Provision for having maternity relief",
+    "43": "There should be defined wages for a daily wage worker",
+    "43A": "Government should allow industries to have Union or Workers Association",
+    "43B": "The state shall endeavour to promote voluntary formation, autonomous functioning, democratic control and professional management of co-operative societies",
+    "44": "Civil Code (Laws) are same for everyone in the country",
+    "45": "Free and compulsory education for every children below the age of 14",
+    "46": "Promotion of educational and economic interests of SC/ST and other weaker sections of the society",
+    "47": "Duty of the state to raise the nutrition level and standard of living. Duty of the state to improve the public health",
+    "48": "Duty of the state to have organisation for agriculture and animal husbandary",
+    "48A": "Protection and improvement of environment and safe gaurding of forests and wildlife. rvind Kejriwal’s government was charged Rs. 5 Crore penalty by NGT for violating (not following the instructions) in Artcile 48 and Article 48(A). Article 48 and 48(A) are asked many times in SSC",
+    "49": "Protection of monuments, places and objects of national importance",
+    "50": "Seperation of judiciary from the executive",
+    "51": "Promotion of international peace and security",
+
+    #Articles that are not part of Directive Principles of State Policy, but they are directives
+    "350(A)": "Primary education in mother tongue",
+    "351": "Promotion of Hindi"
 
 }
